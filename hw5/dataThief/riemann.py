250670e6984d8d704c949115dca2a1c422d0c7a7
Galen Cochrane <a.day.0ld.bag3l@gmail.com>
2019-02-08 09:00:15 -0700

    fixed integration

--- hw5/dataThief/riemann.py
+++ hw5/dataThief/riemann.py
@@ -1,5 +1,12 @@
+
 import numpy
-data = numpy.loadtxt('dataThief.txt')
-#print(data[7:,1].sum())
-print(data)
+import scipy.integrate
+
+data = numpy.loadtxt('dataThief.txt', delimiter=',')
+inverted_ys = []
+for y in data[:,1]:
+    inverted_ys.append(1.0 / y)
+simp = scipy.integrate.simps(x=data[:,0], y=inverted_ys)
+simp /= 8.962
+print(simp)
 
